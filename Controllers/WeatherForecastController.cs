c7e407c08b1dce7fac3403ac0d83c9e5bd9c7cd3
Bartosz Żuber 242574 <jakisbartosz@gmail.com>
2024-08-29 13:17:36 +0200

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+      
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWeatherForecastService _service;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var result = _service.Get();
+            return result;
+        }
+
+
+        [HttpGet("currentDay/{max}")]
+        
+        public IEnumerable<WeatherForecast> Get2([FromQuery]int take, [FromRoute]int max)
+        {
+            var result = _service.Get();
+            return result;
+        }
+        [HttpPost]
+        public ActionResult<string> Hello([FromBody] string name)
+        {
+            HttpContext.Response.StatusCode = 401;
+            return $"Hello {name}";
+        }
+    }
+}
