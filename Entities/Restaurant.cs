94f46e69e1319ff9e593239936ab586443865c7e
Bartosz Żuber 242574 <jakisbartosz@gmail.com>
2024-08-30 17:01:59 +0200

    migrations addded

---
+++ Entities/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿namespace RestaurantAPI.Entities
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public String Description { get; set; }
+
+        public string Category { get; set; }
+
+        public bool HasDelivery {  get; set; }
+
+        public string Email { get; set; }
+
+        public int AddressId { get; set; }
+
+        public virtual Address Address { get; set; }
+
+        public virtual ICollection<Dish> Dishes { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using RestaurantAPI;
+using RestaurantAPI.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,9 +9,19 @@ builder.Services.AddControllers();
 
 builder.Services.AddTransient<IWeatherForecastService, WeatherForecastService>();
 
+builder.Services.AddDbContext<RestaurantDbContext>();
+
+builder.Services.AddScoped<RestaurantSeeder>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
+    seeder.Seed();
+}
 
 app.UseHttpsRedirection();
 
